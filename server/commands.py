d469dc0320f5514978fb3ab3d0617693a68aef72
Jina Zhu <jinazhu87@gmail.com>
2020-11-19 15:45:26 -0800

    create question database

--- server/commands.py
+++ server/commands.py
@@ -1,5 +1,14 @@
 import click
 from flask.cli import with_appcontext
+from .extensions import db
+from .model import Question
+
+@click.command(name="create_tables")
+@with_appcontext
+def create_tables():
+    db.create_all()
+    print('db', db)
+    print("tables created")
 
 @click.command(name="say_hi")
 @with_appcontext
---
+++ server/model.py
@@ -0,0 +1,12 @@
+from .extensions import db
+
+class Question(db.Model):
+    __tablename__ = "questions"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    question = db.Column(db.String, nullable=False)
+    option = db.Column(db.String, nullable=False)
+    answer = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"<question={self.question}>"
