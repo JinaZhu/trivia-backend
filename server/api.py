afdfe58f6d8a36560a496395d96f952583bc70a4
Jina Zhu <jinazhu87@gmail.com>
2020-11-19 20:41:10 -0800

    add new route

--- server/api.py
+++ server/api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request 
 from flask_cors import CORS
+import sys
 
 from .model import Question
 from .extensions import db
@@ -12,15 +13,14 @@ CORS(api)
 def say_hello():
     return jsonify("hello")
 
-@api.route('/api/addQuestion', methods=["POST"])
+@api.route('/api/addQuestion', methods=["POST", "GET"])
 def add_question():
     """add new question to database"""
-    
+
     new_question = request.get_json()
     question = new_question["question"]
     options = (",").join(new_question["options"])
     answer = new_question["answer"]
-
 
     if not question or not options or not answer: 
         return jsonify("There was an issue adding a question, please provide a valid question with options and an answer"), 400
@@ -32,4 +32,5 @@ def add_question():
         db.session.commit()
         return jsonify('Thank you! You question has been added.'), 200
     except: 
+        print("Unexpected error:", sys.exc_info()[0])
         return jsonify("There was an issue adding a question, please try again.")
